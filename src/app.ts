41f3ef4f116dc7ca18b20c7cb8a73264a904e87b
her85 <herbotto1985@gmail.com>
2026-01-14 16:12:51 -0300

    fix: actualizar configuración de CORS para permitir orígenes específicos

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,12 @@ const app = express();
 app.use(helmet());
 
 // Middlewares base
-app.use(cors());
+app.use(cors({
+  origin: [
+    'https://front-sistema-de-gestion-de-siniestros.onrender.com',
+    'http://localhost:9000'
+  ]
+}));
 app.use(express.json({ limit: '10mb' })); // Limitar tamaño de payload
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
